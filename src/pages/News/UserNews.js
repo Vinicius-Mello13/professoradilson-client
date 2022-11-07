85cd0b9a70216ad03e3592071ce5a82f9f1bbf5d
Erick Kogake <erick.kogake@aigroup.local>
2022-11-07 08:19:34 -0300

    ok

--- src/pages/News/UserNews.js
+++ src/pages/News/UserNews.js
@@ -73,11 +73,44 @@ export function UserNews() {
   }, []);
 
   return (
-    <div>
-      <div>
-        <img />
-        <div></div>
+    <div style={{
+      backgroundColor: 'blue',
+      position: 'fixed',
+      width: '100%',
+      height: '90px',
+      justifyContent: 'space-between',
+      boxShadow: '0px 1px 1px black',
+      display: 'flex',
+    }}>
+      <div className='nav-bar'>
+        <img src='https://iili.io/poFqP4.png' alt='Brasão Barretos'
+          style={{
+            width: '100px',
+            height: '75px',
+         
+          }}></img>
       </div>
-    </div>
+      <div className='nav-bar' style={{
+        border: '2px solid black',
+        borderRadius: '0 0 45px 45px',
+        borderTop: 'hidden',
+        backgroundColor: 'white',
+        boxShadow: '0px 5px 5px 2px black'
+      }}>
+        <p style={{
+          fontWeight: 'bold',
+          fontSize: '25px',
+          fontFamily: 'verdana'
+        }}>
+          NOTÍCIAS
+        </p>
+      </div>
+      <div className='nav-bar'>
+        <ul>
+          <li><a href='#' style={{color:'white',fontSize:'12px', fontWeight:'bold', textDecoration:'none'}}><p>MATÉRIAS</p></a></li>
+          <li><a href='#' style={{color:'white',fontSize:'12px', fontWeight:'bold',textDecoration:'none'}}><p>SUGESTÕES</p></a></li>
+        </ul>
+      </div>
+    </div >
   );
 }
